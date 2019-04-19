e2370b711d9591de1b7f068c3c1ea0675e985654
beefchimi <curtdulmage@gmail.com>
2019-04-19 17:41:54 -0400

    set aside htaccess

--- tools/tasks/root.js
+++ tools/tasks/root.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 
 const srcRoot = 'src/root/';
 const distRoot = 'dist/';
-const htaccess = `${srcRoot}.htaccess`;
+const htaccess = `${srcRoot}.+(htaccess)`;
 
 export function root() {
   return gulp.src([`${srcRoot}*`, htaccess]).pipe(gulp.dest(distRoot));
