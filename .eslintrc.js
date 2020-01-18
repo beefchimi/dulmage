893475f3d784ffc1413af7d83cd64a15466736b1
beefchimi <curtdulmage@gmail.com>
2020-01-18 17:14:23 -0500

    :arrow_up: [Revisit] Bumping all files after a long hiatus

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
 module.exports = {
-  extends: [
-    // 'plugin:shopify/eslint-comments',
-    'plugin:shopify/esnext',
-    // 'plugin:shopify/node',
-    'plugin:shopify/webpack',
-    'plugin:shopify/prettier',
-  ],
+  extends: ['plugin:shopify/esnext', 'plugin:shopify/webpack', 'plugin:shopify/prettier'],
   globals: {},
   rules: {
-    'no-console': 0,
-    'no-warning-comments': 0,
+    'shopify/jsx-no-complex-expressions': 'off',
     'eslint-comments/no-unlimited-disable': 0,
-    'import/no-cycle': 0,
+    'import/no-default-export': ['error'],
+    'lines-around-comment': [
+      'error',
+      {
+        beforeBlockComment: false,
+        allowBlockStart: false,
+      },
+    ],
+    'no-console': 0,
+    'no-negated-condition': 'off',
+    // We are intentionally keeping `TODO` comments until a stable release
+    'no-warning-comments': 'off',
+    // TODO: Disabling for now, but we will want to re-enable in the future.
+    'import/no-default-export': 'off',
+    'import/no-cycle': 'off',
   },
   env: {
     browser: true,
